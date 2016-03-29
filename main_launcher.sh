3baa0460047752e4366df4d5b75389612d0cdc76
Marc DELAGE <mdelage@student.42.fr>
2016-03-29 18:25:47 +0200

    add main script

---
+++ main_launcher.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+scripts_relative_path="scripts_enabled"
+
+if [[ "${0:0:1}" == "/" ]]
+then
+    scripts_path="`dirname $0`/$scripts_relative_path"
+else
+    scripts_path="`pwd`/`dirname $0`/$scripts_relative_path"
+fi
+
+if [[ `id -u` -ne 0 ]]
+then
+    echo "Need to be run as root"
+    exit 1
+fi
+
+if [[ ! -e "$scripts_relative_path" ]]
+then
+    mkdir -p "$scripts_relative_path"
+    echo "It appear to be the first time you run this script."
+    echo "Put in $scripts_path the scripts or symbolic link to your scripts"
+else
+    for script in `find "$scripts_path" -mindepth 1`
+    do
+        $script
+    done
+fi
+
